3c982b100fd82959d4f9e8ecf0f93930a75a7136
Ryan Goeken <ryan.goeken@gmail.com>
2018-04-03 15:16:59 -0500

    Version 0.7

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	e := echo.New()
 	e.HideBanner = true
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "!Dlrow, olleH\nVersion 0.5")
+		return c.String(http.StatusOK, "Hello, World!\nVersion 0.7")
 	})
 	e.Logger.Fatal(e.Start(":1337"))
 }
